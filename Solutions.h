e9f6f333e1cbd208686879cc1781d9395170677c
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2023-03-14 21:09:46 -0400

    added sumNumbers to Solutions class

--- Solutions.h
+++ Solutions.h
@@ -3020,6 +3020,32 @@ public:
         return result;
     }
 
+    /**
+     * Given the root of a binary tree containing digits from 0 to 9 only. Each
+     * root-to-leaf path in the tree represents a number.
+     * For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+     * Return the total sum of all root-to-leaf numbers. Test cases are
+     * generated so that the answer will fit in a 32-bit integer.
+     * A leaf node is a node with no children.
+     * @param root the root of the binary tree
+     * @return int the total sum of all root-to-leaf numbers
+     */
+     int sumNumbers(TreeNode* root) {
+        return sumNumbers(root, 0);
+    }
+    // sumNumbers helper function
+    int sumNumbers(TreeNode* root, int sum) {
+        if (!root) {
+            return 0;
+        }
+        sum = sum * 10 + root->val;
+        if (!root->left && !root->right) {
+            return sum;
+        }
+        return sumNumbers(root->left, sum) + sumNumbers(root->right, sum);
+    }
+
+
 
 };
 
