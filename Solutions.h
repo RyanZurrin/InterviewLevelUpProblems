2f76ef94365ad1fd97958eb78f23f5aba23be052
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2023-03-19 21:38:08 -0400

    added canPlaceFlower solution

--- Solutions.h
+++ Solutions.h
@@ -3046,6 +3046,43 @@ public:
         return sumNumbers(root->left, sum) + sumNumbers(root->right, sum);
     }
 
+    /**
+     * You have a long flowerbed in which some of the plots are planted, and
+     * some are not. However, flowers cannot be planted in adjacent plots.
+     * Given an integer array flowerbed containing 0's and 1's, where 0 means
+     * empty and 1 means not empty, and an integer n, return if n new flowers
+     * can be planted in the flowerbed without violating the no-adjacent-flowers
+     * rule.
+     * @param flowerbed the flowerbed array
+     * @param n the number of flowers to be planted
+     * @return bool true if n new flowers can be planted in the flowerbed
+     * without violating the no-adjacent-flowers rule, else false
+     */
+     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        int m = flowerbed.size();
+        if (m == 1) {
+            return (flowerbed[0] == 0 && n <= 1) || (flowerbed[0] == 1 && n == 0);
+        }
+        if (m == 2) {
+            return (flowerbed[0] == 0 && flowerbed[1] == 0 && n <= 1) ||
+                   (flowerbed[0] == 1 && flowerbed[1] == 0 && n == 0) ||
+                   (flowerbed[0] == 0 && flowerbed[1] == 1 && n == 0);
+        }
+        for (int i = 0; i < m && count < n; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+            int prev = (i == 0) ? 0 : flowerbed[i - 1];
+            int next = (i == m - 1) ? 0 : flowerbed[i + 1];
+            if (prev == 0 && next == 0) {
+                flowerbed[i] = 1;
+                count++;
+            }
+        }
+        return count == n;
+    }
+
 
 
 };
