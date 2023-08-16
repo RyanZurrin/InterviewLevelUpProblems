ee1b75eb1a74033140400cb9256d27944616a745
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2023-08-15 20:07:39 -0400

    Added Partition List and The Maze to Solutions.h

--- Solutions.h
+++ Solutions.h
@@ -4345,8 +4345,131 @@ public:
         return res;
     }
 
+    /**
+     * 86. Partition List
+     * Given the head of a linked list and a value x, partition it such that
+     * all nodes less than x come before nodes greater than or equal to x.
+     * You should preserve the original relative order of the nodes in each
+     * of the two partitions.
+     *
+     * Example 1:
+     * Input: head = [1,4,3,2,5,2], x = 3
+     * Output: [1,2,2,4,3,5]
+     *
+     * Example 2:
+     * Input: head = [2,1], x = 2
+     * Output: [1,2]
+     *
+     * Constraints:
+     * The number of nodes in the list is in the range [0, 200].
+     * -100 <= Node.val <= 100
+     * -200 <= x <= 200
+     *
+     * @param head the head of the linked list
+     * @param x the value
+     * @return the partitioned linked list
+     */
+    static ListNode* partition(ListNode* head, int x) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* pre = dummy;
+        ListNode* cur = head;
+        while (cur && cur->val < x) {
+            pre = cur;
+            cur = cur->next;
+        }
+        ListNode* p = cur;
+        while (p && p->next) {
+            if (p->next->val < x) {
+                ListNode* tmp = p->next;
+                p->next = tmp->next;
+                tmp->next = cur;
+                pre->next = tmp;
+                pre = tmp;
+            } else {
+                p = p->next;
+            }
+        }
+        return dummy->next;
+    }
 
+    /**
+     * 490. The Maze
+     * There is a ball in a maze with empty spaces (represented as 0) and walls
+     * (represented as 1). The ball can go through the empty spaces by rolling
+     * up, down, left or right, but it won't stop rolling until hitting a wall.
+     * When the ball stops, it could choose the next direction.
+     * Given the m x n maze, the ball's start position and the destination,
+     * where start = [startrow, startcol] and destination = [destinationrow,
+     * destinationcol], return true if the ball can stop at the destination,
+     * otherwise return false.
+     * You may assume that the borders of the maze are all walls (see examples).
+     *
+     * Example 1:
+     * Input: maze = [[0,0,1,0,0], [0,0,0,0,0], [0,0,0,1,0], [1,1,0,1,1],
+     * [0,0,0,0,0]], start = [0,4], destination = [4,4]
+     * Output: true
+     * Explanation: One possible way is : left -> down -> left -> down ->
+     * right -> down -> right.
+     *
+     * Example 2:
+     * Input: maze = [[0,0,1,0,0], [0,0,0,0,0], [0,0,0,1,0], [1,1,0,1,1],
+     * [0,0,0,0,0]], start = [0,4], destination = [3,2]
+     * Output: false
+     * Explanation: There is no way for the ball to stop at the destination.
+     * Notice that you can pass through the destination but you cannot stop there.
+     *
+     * Example 3:
+     * Input: maze = [[0,0,0,0,0], [1,1,0,0,1], [0,0,0,0,0], [0,1,0,0,1],
+     * [0,1,0,0,0]], start = [4,3], destination = [0,1]
+     * Output: false
+     *
+     * Constraints:
+     * m == maze.length
+     * n == maze[i].length
+     * 1 <= m, n <= 100
+     * maze[i][j] is 0 or 1.
+     * start.length == 2
+     * destination.length == 2
+     * 0 <= startrow, destinationrow <= m
+     * 0 <= startcol, destinationcol <= n
+     * Both the ball and the destination exist in an empty space, and they will
+     * not be in the same position initially.
+     * The maze contains at least 2 empty spaces.
+     *
+     * @param maze the maze
+     * @param start the start position
+     * @param destination the destination
+     * @return whether the ball can stop at the destination
+     */
+    static bool hasPath(vector<vector<int>>& maze, vector<int>& start,
+                        vector<int>& destination) {
+        int m = maze.size();
+        int n = maze[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        return dfs(maze, start[0], start[1], destination, visited);
+    }
 
+    static bool dfs(vector<vector<int>>& maze, int i, int j,
+                    vector<int>& destination, vector<vector<bool>>& visited) {
+        if (visited[i][j]) return false;
+        if (i == destination[0] && j == destination[1]) return true;
+        visited[i][j] = true;
+        int m = maze.size();
+        int n = maze[0].size();
+        int up = i;
+        while (up > 0 && maze[up - 1][j] == 0) up--;
+        int down = i;
+        while (down < m - 1 && maze[down + 1][j] == 0) down++;
+        int left = j;
+        while (left > 0 && maze[i][left - 1] == 0) left--;
+        int right = j;
+        while (right < n - 1 && maze[i][right + 1] == 0) right++;
+        return dfs(maze, up, j, destination, visited) ||
+               dfs(maze, down, j, destination, visited) ||
+               dfs(maze, i, left, destination, visited) ||
+               dfs(maze, i, right, destination, visited);
+    }
 
 
 
--- leetCode.cpp
+++ leetCode.cpp
@@ -14,14 +14,46 @@ typedef Solutions S;
 
 int main()
 {
-    string expression = "T?2:3";
-    printf("%s\n", S::parseTernary(expression).c_str());
+    /** Test this function
+     *     /**
+     * 86. Partition List
+     * Given the head of a linked list and a value x, partition it such that
+     * all nodes less than x come before nodes greater than or equal to x.
+     * You should preserve the original relative order of the nodes in each
+     * of the two partitions.
+     *
+     * Example 1:
+     * Input: head = [1,4,3,2,5,2], x = 3
+     * Output: [1,2,2,4,3,5]
+     *
+     * Example 2:
+     * Input: head = [2,1], x = 2
+     * Output: [1,2]
+     *
+     * Constraints:
+     * The number of nodes in the list is in the range [0, 200].
+     * -100 <= Node.val <= 100
+     * -200 <= x <= 200
+     */
+    ListNode* head = new ListNode(1);
+    ListNode* head2 = new ListNode(4);
+    ListNode* head3 = new ListNode(3);
+    ListNode* head4 = new ListNode(2);
+    ListNode* head5 = new ListNode(5);
+    ListNode* head6 = new ListNode(2);
+    head->next = head2;
+    head2->next = head3;
+    head3->next = head4;
+    head4->next = head5;
+    head5->next = head6;
+    ListNode* ans = S::partition(head, 3);
+    while (ans != nullptr)
+    {
+        std::cout << ans->val << " ";
+        ans = ans->next;
+    }
+    std::cout << std::endl;
 
-    expression = "F?1:T?4:5";
-    printf("%s\n", S::parseTernary(expression).c_str());
-
-    expression = "T?T?F:5:3";
-    printf("%s\n", S::parseTernary(expression).c_str());
 
 
 
